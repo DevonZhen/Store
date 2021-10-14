333923017013478e0dfd25c4e1a1d7b554974970
Devon Zhen <zhen.devon@gmail.com>
2021-10-14 15:11:53 -0400

    Orders

--- src/main/java/com/store/repository/CustomerQueryRepository.java
+++ src/main/java/com/store/repository/CustomerQueryRepository.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import com.store.domain.Orders;
 import com.store.dto.CustomersDTO;
 import com.store.dto.CustomersFormData;
 import com.store.dto.OrderItemsDTO;
@@ -47,6 +49,7 @@ public class CustomerQueryRepository {
         	custList = jdbcTemplate.query(qq_SQL, new CustomersMapper());
 	
         	customersList = parseToCustomerDTO(custList);
+
 
         }
         catch (Exception ex) {
@@ -85,8 +88,8 @@ public class CustomerQueryRepository {
 	    		 orderItemsDTO.setPrice(rs.getDouble("Price"));
 	    		 orderItemsList.add(orderItemsDTO);
 	    		 
-	    	  ordersDTO.setOrderItems(orderItemsList);
-    		  ordersList.add(ordersDTO);
+	    	ordersDTO.setOrderItems(orderItemsList);
+    		ordersList.add(ordersDTO);
    		    customersDTO.setOrders(ordersList);
     		
     		return customersDTO;
@@ -168,20 +171,106 @@ public class CustomerQueryRepository {
     	
     	CustomersDTO customersDTO = null;
     	try {
-            System.out.println("## customersDTO=="+customersFormData);
+            System.out.println("Updating section customersDTO == "+customersFormData);
             customersDTO = customersFormData.getCustomersDTO();
             
-			//1. Find the remain orders for update
+			//1. Find the remain orders for UPDATE
+            for(OrdersDTO o : customersDTO.getOrders())  {
+            	try {
+            		if(o.getOrderId()!=null) {
+	            		String SQL_Update_Orders="UPDATE public.\"Orders\" "+
+	            				"SET \"Order_Id\" = "+o.getOrderId()+", \"Order_Status\" = '"+o.getOrderStatus()+
+	            				"', \"Order_Date\" = '"+o.getOrderDate()+"', \"Store_Id\" = "+o.getStoreId()+" "+
+	            				"WHERE \"Order_Id\" = "+o.getOrderId();
+	            		for(OrderItemsDTO i: o.getOrderItems()) {
+	            			String SQL_Update_OrderItems="UPDATE public.\"Order_Items\""+
+    	                    		"SET \"Order_Items_Id\" = "+i.getOrderItemsId()+", \"Item\" = "+i.getItem()+
+    	                    		", \"Quantity\" = "+i.getQuantity()+", \"Price\" = "+i.getPrice()+", \"Order_Id\" = "+i.getOrderId();
+	            			jdbcTemplate.update(SQL_Update_OrderItems, new CustomersMapper());
+	            			System.out.println("Updating Order Items Completed");
+	            		}
+	            		
+	            		System.out.println("Update This One..."+o.getOrderId());
+//	            		jdbcTemplate.update(SQL_Update_Orders, new CustomersMapper());
+            		}
+            	}catch(Exception e) {
+            		e.printStackTrace();
+            	}
+            }
 
-	        //2. Find the removed orders for delete
+            
+	        //2. Find the removed orders for DELETE
+            String SQL_Delete_Orders="DELETE FROM public.\"Orders\" WHERE \"Order_Id\" = "+customersFormData.getDelOrderIds();
+            String SQL_Delete_OrderItems="DELETE FROM public.\"Orders_Items\" WHERE \"Order_Items_Id\" = "+customersFormData.getDelItemIds();
+            try {
+        		System.out.println("Deleting Orders..."+customersFormData.getDelOrderIds());
+//            	jdbcTemplate.update(SQL_Delete_Orders, new CustomersMapper());
+            	System.out.println("Deleting Order Items..."+customersFormData.getDelItemIds());
+//            	jdbcTemplate.update(SQL_Delete_OrderItems, new CustomersMapper());
+            	System.out.println("Deletion Completed...");
 
-	        //3. Find the new orders for insert
-
+        		
+        	}catch(Exception e) {
+        		e.printStackTrace();
+        	}
+            	
+            
+            
+            
+	        //3. Find the new orders for INSERT   
+            for(OrdersDTO o : customersDTO.getOrders())  {
+            	if(o.getOrderId()==null) {
+            		
+            		long orderSeqNum = jdbcTemplate.queryForObject("SELECT nextval('public.\"Seq_Orders\"');", Integer.class);
+            		long itemSeqNum  = jdbcTemplate.queryForObject("SELECT nextval('public.\"Seq_Order_Items\"');", Integer.class);
+            		
+                    String SQL_Insert_Orders="INSERT INTO public.\"Orders\""+
+                    		"(\"Order_Id\", \"Order_Status\", \"Order_Date\", \"Customer_Id\", \"Store_Id\") "+
+                    		"VALUES("+orderSeqNum+", '"+o.getOrderStatus()+"', '"+o.getOrderDate()+"', 4, "+o.getStoreId()+") ";
+                    try {
+                		System.out.println("++++++++++++++ Inserting Orders... "+o);
+//                		jdbcTemplate.update(SQL_Insert_Orders);
+                		for(OrderItemsDTO i: o.getOrderItems()) {
+    	                    String SQL_Insert_OrderItems="INSERT INTO public.\"Order_Items\""+
+    	                    		"(\"Order_Items_Id\", \"Item\", \"Quantity\", \"Price\", \"Order_Id\") "+
+    	                    		"VALUES("+itemSeqNum+", '"+i.getItem()+"', '"+i.getQuantity()+"', '"+i.getPrice()+"', '"+orderSeqNum+") ";
+    	                    System.out.println(">>>>>>>> Inserting Order Items... "+i);
+//    	            		jdbcTemplate.update(SQL_Insert_OrderItems);
+//    	                    i.setOrderItemsId(itemSeqNum);
+//    	                    i.setOrderId(itemSeqNum);
+                        }
+                		System.out.println("Inserting Completed... ");
+//                    	o.setOrderId(orderSeqNum);
+                    	
+                	}catch(Exception e) {e.printStackTrace();}
+            	}
+            	else {
+            		for(OrderItemsDTO i : o.getOrderItems()) {
+                		if(o.getOrderId()!=null && i.getOrderItemsId()==null) {
+                			System.out.println("<<<<<<<<<< Insert Order Item "+i);
+                			long orderSeqNum = jdbcTemplate.queryForObject("SELECT nextval('public.\"Seq_Orders\"');", Integer.class);
+                    		long itemSeqNum  = jdbcTemplate.queryForObject("SELECT nextval('public.\"Seq_Order_Items\"');", Integer.class);
+                			String SQL_Insert_OrderItems="INSERT INTO public.\"Order_Items\""+
+    	                    		"(\"Order_Items_Id\", \"Item\", \"Quantity\", \"Price\", \"Order_Id\") "+
+    	                    		"VALUES("+itemSeqNum+", '"+i.getItem()+"', '"+i.getQuantity()+"', '"+i.getPrice()+"', '"+orderSeqNum+") ";
+//    	            		jdbcTemplate.update(SQL_Insert_OrderItems);
+                		}
+                		else
+                			System.out.println("Skip Insert Order Item #"+i.getOrderItemsId());
+                	}
+            	}
+            	
+            	
+            }  
+ 
+           	return customersDTO;
+           		 
     		
     	} catch (Exception e) {
     		 e.printStackTrace();
     	}
 		return customersDTO;
     }
+    
  
 }
--- src/test/java/com/store/junit/SpringBootApplicationJunitTest.java
+++ src/test/java/com/store/junit/SpringBootApplicationJunitTest.java
@@ -53,78 +53,112 @@ public class SpringBootApplicationJunitTest {
 			//initialize Customer
 			CustomersDTO customersDTO = new CustomersDTO();
 
-			customersDTO.setCustomerId(Long.valueOf(2));
-			customersDTO.setFirstName("Jared");
-		    customersDTO.setLastName("Volo");
-		    customersDTO.setPhone("6098463251");
-		    customersDTO.setEmail("jvee22@gmail.com");
-		    customersDTO.setStreet("2 Roko Road");
-		    customersDTO.setZip(Long.valueOf(96328));
+			customersDTO.setCustomerId(Long.valueOf(4));
+			customersDTO.setFirstName("Mako");
+		    customersDTO.setLastName("Loan");
+		    customersDTO.setPhone("6091239476");
+		    customersDTO.setEmail("malo609@gmail.com");
+		    customersDTO.setStreet("6 Turken Road");
+		    customersDTO.setZip(Long.valueOf(84629));
 		    
 		    List<OrdersDTO> ordersDTOlist = new ArrayList<>(); 
 		    //initialize Orders 1
 		    OrdersDTO ordersDTO_1 = new OrdersDTO();
-		    ordersDTO_1.setOrderId(Long.valueOf(130));
+		    ordersDTO_1.setOrderId(Long.valueOf(117));
 		    ordersDTO_1.setOrderStatus("Canceled");
 		    SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy-MM-dd");
-		    ordersDTO_1.setOrderDate(formatter1.parse("2020-01-11"));
-		    ordersDTO_1.setStoreId(Long.valueOf(2));
+		    ordersDTO_1.setOrderDate(formatter1.parse("2015-3-15"));
+		    ordersDTO_1.setStoreId(Long.valueOf(3));
 		    
 		    List<OrderItemsDTO> orderItemDTOList1 = new ArrayList<>(); 
 		    //initialize Orders_1 Items_1
 		    OrderItemsDTO orderItemsDTO11 = new OrderItemsDTO();
-		    orderItemsDTO11.setOrderItemsId(Long.valueOf(19));
-		    orderItemsDTO11.setItem("RAM");
-		    orderItemsDTO11.setQuantity(Long.valueOf(5));
-		    orderItemsDTO11.setPrice(Double.valueOf(86.59));
+		    orderItemsDTO11.setOrderItemsId(Long.valueOf(14));
+		    orderItemsDTO11.setItem("Phone");
+		    orderItemsDTO11.setQuantity(Long.valueOf(1));
+		    orderItemsDTO11.setPrice(Double.valueOf(1200.00));
 		    orderItemDTOList1.add(orderItemsDTO11);	    
-		    
-		    //initialize Orders_1 Items_2
-		    /*
+		  //initialize Orders_1 Items_2
 		    OrderItemsDTO orderItemsDTO12 = new OrderItemsDTO();
-		    orderItemsDTO12.setOrderItemsId(Long.valueOf(2));
-		    orderItemsDTO12.setItem("Desktop");
-		    orderItemsDTO12.setQuantity(Long.valueOf(3));
-		    orderItemsDTO12.setPrice(Double.valueOf(94.33));
-		    orderItemsDTO12.setOrderId(Long.valueOf(84962));		    
-		    orderItemDTOList1.add(orderItemsDTO12);	 
-		    */   
+		    orderItemsDTO12.setOrderItemsId(null);
+		    orderItemsDTO12.setItem("TestD");
+		    orderItemsDTO12.setQuantity(Long.valueOf(333));
+		    orderItemsDTO12.setPrice(Double.valueOf(3333.00));
+		    orderItemDTOList1.add(orderItemsDTO12);	  
 		    
 		    ordersDTO_1.setOrderItems(orderItemDTOList1);
 		    ordersDTOlist.add(ordersDTO_1);
-		    
+
 		    //initialize Orders_2 
 		    OrdersDTO ordersDTO_2 = new OrdersDTO();
-		    ordersDTO_2.setOrderId(null);
+		    ordersDTO_2.setOrderId(Long.valueOf(7456));
 		    ordersDTO_2.setOrderStatus("Completed");
 		    SimpleDateFormat formatter2 = new SimpleDateFormat("yyyy-MM-dd");
-		    ordersDTO_2.setOrderDate(formatter2.parse("2021-11-01"));
-		    ordersDTO_2.setStoreId(Long.valueOf(2));
+		    ordersDTO_2.setOrderDate(formatter2.parse("2021-10-02"));
+		    ordersDTO_2.setStoreId(Long.valueOf(3));
 		    
 		    List<OrderItemsDTO> orderItemDTOList2 = new ArrayList<>(); 
 		    //initialize Orders_2 Items_1
 		    OrderItemsDTO orderItemsDTO21 = new OrderItemsDTO();
-		    orderItemsDTO21.setOrderItemsId(null);
-		    orderItemsDTO21.setItem("Computer");
+		    orderItemsDTO21.setOrderItemsId(Long.valueOf(15));
+		    orderItemsDTO21.setItem("Case");
 		    orderItemsDTO21.setQuantity(Long.valueOf(1));
-		    orderItemsDTO21.setPrice(Double.valueOf(125.99));
-		    orderItemsDTO21.setOrderId(Long.valueOf(8888));
+		    orderItemsDTO21.setPrice(Double.valueOf(500.36));
+		    orderItemsDTO21.setOrderId(Long.valueOf(118));
 		    orderItemDTOList2.add(orderItemsDTO21);	
+		    //initialize Orders_2 Items_2  <-- Order ID YES Order Item ID NO
+		    OrderItemsDTO orderItemsDTO22 = new OrderItemsDTO();
+		    orderItemsDTO22.setOrderItemsId(null);
+		    orderItemsDTO22.setItem("TestC");
+		    orderItemsDTO22.setQuantity(Long.valueOf(122));
+		    orderItemsDTO22.setPrice(Double.valueOf(84.11));
+		    orderItemsDTO22.setOrderId(null);
+		    orderItemDTOList2.add(orderItemsDTO22);
 		    
 		    ordersDTO_2.setOrderItems(orderItemDTOList2);
 		    ordersDTOlist.add(ordersDTO_2);
 		    
+		    //initialize Orders 3 <-- Order ID NO Order Item ID NO
+		    OrdersDTO ordersDTO_3 = new OrdersDTO();
+		    ordersDTO_3.setOrderId(null);
+		    ordersDTO_3.setOrderStatus("Waiting");
+		    SimpleDateFormat formatter3 = new SimpleDateFormat("yyyy-MM-dd");
+		    ordersDTO_3.setOrderDate(formatter3.parse("1996-4-16"));
+		    ordersDTO_3.setStoreId(Long.valueOf(2));
+		    ordersDTOlist.add(ordersDTO_3);	
+		    //Order Item 3-1 <-- Order ID NO Order Item ID NO
+		    List<OrderItemsDTO> orderItemDTOList3 = new ArrayList<>();
+		    OrderItemsDTO orderItemsDTO31 = new OrderItemsDTO();
+		    orderItemsDTO31.setOrderItemsId(null);
+		    orderItemsDTO31.setItem("TestA");
+		    orderItemsDTO31.setQuantity(Long.valueOf(12));
+		    orderItemsDTO31.setPrice(Double.valueOf(1.36));
+		    orderItemsDTO31.setOrderId(null);
+		    orderItemDTOList3.add(orderItemsDTO31);
+		    //Order Item 3-2
+//		    OrderItemsDTO orderItemsDTO32= new OrderItemsDTO();
+//		    orderItemsDTO32.setOrderItemsId(null);
+//		    orderItemsDTO32.setItem("TestB");
+//		    orderItemsDTO32.setQuantity(Long.valueOf(3));
+//		    orderItemsDTO32.setPrice(Double.valueOf(46.36));
+//		    orderItemsDTO32.setOrderId(null);
+//		    orderItemDTOList3.add(orderItemsDTO32);
+		    
+		    ordersDTO_3.setOrderItems(orderItemDTOList3);
+		    
 		    customersDTO.setOrders(ordersDTOlist);
 		    
-		    customersFormData.setDelItemIds("");
-		    customersFormData.setDelOrderIds("132");
+		    customersFormData.setDelItemIds("15");
+		    customersFormData.setDelOrderIds("125");
 		    customersFormData.setCustomersDTO(customersDTO);
 		    
 		    ResponseEntity<CustomersDTO> customerDTO = customerQueryController.updateCustomers(customersFormData);
 
-			System.out.println("####=="+customerDTO.getBody().getFirstName());
+//		    List<CustomersDTO> customerDTOList = customerQueryRepository.getCustomersList();	
+//			System.out.println("####=="+customerDTO.getBody().getFirstName());
 			
 		    System.out.println("=============================================");
+//		    System.out.println("New Updated Form: "+customerDTOList);
 	        System.out.println("### CustomerList() Successful: Return "+customerDTO+") ###");
 	        System.out.println("--- Original --- "+customersDTO);
 		    System.out.println("=============================================");
@@ -138,6 +172,12 @@ public class SpringBootApplicationJunitTest {
 			}
 		}
 	}	
+	
+	
+	
+	
+	
+	
 	
 	//@Test
 	public void testFindById() 
@@ -192,8 +232,9 @@ public class SpringBootApplicationJunitTest {
 
 			System.out.println("=====================================================");
 	        System.out.println("### getCustomerDTOList() Successful: Return "+customerDTOList.size()+") ###");
-		    System.out.println("=====================================================");
-			
+	        for(CustomersDTO i: customerDTOList)
+	        	System.out.println(i);
+		    System.out.println("=====================================================");   
 		}catch (AssertionError e) {
 			if (e.getMessage() == null) {
 			    System.out.println("==================================================");
